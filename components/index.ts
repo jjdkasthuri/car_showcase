9d65ce2891971b0b3e774cdb644f4827daf67df8
jjdkasthuri <154047882+jjdkasthuri@users.noreply.github.com>
2023-12-21 21:32:24 +0530

    Add files via upload

---
+++ components/index.ts
@@ -0,0 +1,24 @@
+import Hero from './Hero';
+import custombutton from './custombutton';
+import Navbar  from './Navbar';
+import Footer from './Footer';
+import SearchBar from './searchbar';
+import CustomFilter from './customfilter';
+import SearchManufacturer from './SearchManufacturer';
+import carcard from './carcard';
+import cardetails from './cardetails';
+import ShowMore from './Showmore';
+
+
+export {
+    Hero,
+    custombutton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    carcard,
+    cardetails,
+    ShowMore,
+}
